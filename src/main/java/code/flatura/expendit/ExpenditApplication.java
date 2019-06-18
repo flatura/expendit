23a63949868695d8864b700a630782801960ee7b
Dmitry Morozov <flatura@gmail.com>
2019-06-18 13:49:12 +0300

    Added Spring Data JPA beans scan

--- src/main/java/code/flatura/expendit/ExpenditApplication.java
+++ src/main/java/code/flatura/expendit/ExpenditApplication.java
@@ -2,8 +2,10 @@ package code.flatura.expendit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("code.flatura.expendit.repository")
 public class ExpenditApplication {
 
     public static void main(String[] args) {
