288ce5fdabb7a2e69529a12b4997f0928211e24a
Dmitry Morozov <flatura@gmail.com>
2019-07-29 12:33:00 +0300

    DB added independent sequences for all tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,12 +7,25 @@ DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS facility;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS users_seq;
+DROP SEQUENCE IF EXISTS facility_seq;
+DROP SEQUENCE IF EXISTS room_seq;
+DROP SEQUENCE IF EXISTS consumable_type_seq;
+DROP SEQUENCE IF EXISTS consumable_model_seq;
+DROP SEQUENCE IF EXISTS consumable_seq;
+DROP SEQUENCE IF EXISTS consume_fact_seq;
 
-CREATE SEQUENCE global_seq START WITH 100000;
+CREATE SEQUENCE users_seq START WITH 100000;
+CREATE SEQUENCE facility_seq START WITH 100000;
+CREATE SEQUENCE room_seq START WITH 100000;
+CREATE SEQUENCE consumable_type_seq START WITH 100000;
+CREATE SEQUENCE consumable_model_seq START WITH 100000;
+CREATE SEQUENCE consumable_seq START WITH 100000;
+CREATE SEQUENCE consume_fact_seq START WITH 100000;
 
 CREATE TABLE users
 (
-  	id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  	id               INTEGER PRIMARY KEY DEFAULT nextval('users_seq'),
   	name             VARCHAR                 NOT NULL,
   	email            VARCHAR                 NOT NULL,
   	password         VARCHAR                 NOT NULL,
@@ -30,7 +43,7 @@ CREATE TABLE user_roles
 );
 
 CREATE TABLE facility (
-	id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+	id               INTEGER PRIMARY KEY DEFAULT nextval('facility_seq'),
 	title            varchar(255)          NOT NULL,
 	address          varchar(255)          NOT NULL UNIQUE,
 	comments         varchar(255),
@@ -38,7 +51,7 @@ CREATE TABLE facility (
 );
 
 CREATE TABLE room (
-	id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+	id               INTEGER PRIMARY KEY DEFAULT nextval('room_seq'),
 	title            varchar(255)          NOT NULL,
 	facility_id      integer               NOT NULL,
 	storage          boolean               NOT NULL,
@@ -50,13 +63,13 @@ CREATE TABLE room (
 );
 
 CREATE TABLE consumable_type (
-	id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+	id               INTEGER PRIMARY KEY DEFAULT nextval('consumable_type_seq'),
 	name             varchar(255)          NOT NULL UNIQUE,
 	comments         varchar(255)          
 );
 
 CREATE TABLE consumable_model (
-	id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+	id               INTEGER PRIMARY KEY DEFAULT nextval('consumable_model_seq'),
 	name             varchar(30)           NOT NULL,
 	partnumber       varchar(30)           NOT NULL UNIQUE,
 	consumable_type_id  INTEGER            NOT NULL,
@@ -65,23 +78,24 @@ CREATE TABLE consumable_model (
 );
 
 CREATE TABLE consumable (
-	id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+	id               INTEGER PRIMARY KEY DEFAULT nextval('consumable_seq'),
 	name             varchar(255)          NOT NULL,
-	contract	       varchar(255),
+	contract	     varchar(255),
 	price            integer,
 	consumable_model_id integer            NOT NULL,
 	consumable_type_id integer             NOT NULL,
+	room_id			 		 INTEGER			   NOT NULL,
 	status           integer               NOT NULL,
-	FOREIGN KEY (consumable_model_id) REFERENCES consumable_model (id) ON DELETE CASCADE,
-	FOREIGN KEY (consumable_type_id) REFERENCES consumable_type (id) ON DELETE CASCADE
+	FOREIGN KEY (consumable_model_id) REFERENCES consumable_model (id) ON DELETE SET NULL,
+	FOREIGN KEY (consumable_type_id) REFERENCES consumable_type (id) ON DELETE SET NULL
 );
 
 CREATE TABLE consume_fact (
-	id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-	room_id			     INTEGER			        NOT NULL,
-	consumable_id    INTEGER              NOT NULL,
-	consumable_model_id INTEGER          NOT NULL,
-	date             DATE                 NOT NULL,
+	id               INTEGER PRIMARY KEY DEFAULT nextval('consume_fact_seq'),
+	room_id			 INTEGER			   	NOT NULL,
+	consumable_id    INTEGER              	NOT NULL,
+	consumable_model_id INTEGER          	NOT NULL,
+	date             DATE                 	NOT NULL,
 	FOREIGN KEY (room_id) REFERENCES room (id),
-	FOREIGN KEY (consumables_model_id) REFERENCES consumable_model (id)
+	FOREIGN KEY (consumable_model_id) REFERENCES consumable_model (id) ON DELETE SET NULL
 );
