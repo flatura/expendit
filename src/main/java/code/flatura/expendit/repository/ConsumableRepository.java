54e846c167cefbabf0e1804a78c368f8d5b1de2f
Dmitry Morozov <flatura@gmail.com>
2019-12-15 21:16:00 +0300

    Repositories ready

--- src/main/java/code/flatura/expendit/repository/ConsumableRepository.java
+++ src/main/java/code/flatura/expendit/repository/ConsumableRepository.java
@@ -2,10 +2,13 @@ package code.flatura.expendit.repository;
 
 import code.flatura.expendit.model.Consumable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -13,9 +16,13 @@ public interface ConsumableRepository extends JpaRepository<Consumable, Integer>
 
     @Transactional(readOnly = true)
     @Query("SELECT c FROM Consumable c WHERE c.status = 1")
-    List<Consumable> findFull();
+    List<Consumable> getFull();
 
     @Transactional(readOnly = true)
-    @Query("SELECT c FROM Consumable c WHERE c.roomId = roomId")
+    @Query("SELECT c FROM Consumable c WHERE c.roomId = roomId ORDER BY c.name DESC")
     List<Consumable> getByRoomId(int roomId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Consumable c SET c.status = 2, c.roomId = :roomId WHERE (c.status = 1 AND c.id = :id)")
+    void install(@Param("id") int id, @Param("roomId") int roomId);
 }
--- src/main/java/code/flatura/expendit/repository/ConsumeFactRepository.java
+++ src/main/java/code/flatura/expendit/repository/ConsumeFactRepository.java
@@ -3,16 +3,25 @@ package code.flatura.expendit.repository;
 import code.flatura.expendit.model.ConsumeFact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface ConsumeFactRepository extends JpaRepository<ConsumeFact, Integer> {
 
-    @Query("SELECT c FROM ConsumeFact c WHERE c.consumableModelId = id")
-    List<ConsumeFact> getByModel(int id);
+    @Transactional(readOnly = true)
+    @Query("SELECT c FROM ConsumeFact c WHERE c.consumableModelId = :modelId ORDER BY c.date DESC")
+    List<ConsumeFact> getByModelId(@Param("modelId") int id);
 
-    @Query("SELECT c FROM ConsumeFact c WHERE c.roomId = id")
-    List<ConsumeFact> getByRoomId(int id);
+    @Transactional(readOnly = true)
+    @Query("SELECT c FROM ConsumeFact c WHERE c.roomId = :roomId ORDER BY c.date DESC")
+    List<ConsumeFact> getByRoomId(@Param("roomId") int id);
+
+    @Transactional(readOnly = true)
+    @Query("SELECT c from ConsumeFact c WHERE c.date BETWEEN :startDate AND :endDate ORDER BY c.date DESC")
+    List<ConsumeFact> getBetweenDates(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
